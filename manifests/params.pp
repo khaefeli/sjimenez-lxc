6a9dcbab3ae759efb7379d62a3d3b2a572037e25
Kevin Häfeli <kevin@zattoo.com>
2017-07-18 14:22:32 +0200

    remove nat settings

--- manifests/params.pp
+++ manifests/params.pp
@@ -51,21 +51,21 @@ class lxc::params {
   $lxc_cgmanager_service             = 'cgmanager'
   $lxc_cgmanager_service_ensure      = running
   $lxc_cgmanager_service_enabled     = true
-  $lxc_networking_device_link        = 'lxcbr0'
+  $lxc_networking_device_link        = 'br0'
   $lxc_networking_type               = 'veth'
   $lxc_networking_flags              = 'up'
   $lxc_networking_hwaddr             = '00:16:3e:xx:xx:xx'
-  $lxc_networking_nat_enable         = true
-  $lxc_networking_nat_bridge         = 'lxcbr0'
-  $lxc_networking_nat_address        = '10.0.3.1'
-  $lxc_networking_nat_mask           = '255.255.255.0'
-  $lxc_networking_nat_network        = '10.0.3.0/24'
-  $lxc_networking_nat_dhcp_range     = '10.0.3.2,10.0.3.254'
-  $lxc_networking_nat_max_hosts      = '253'
+  $lxc_networking_nat_enable         = false
+  $lxc_networking_nat_bridge         = undef,
+  $lxc_networking_nat_address        = undef,
+  $lxc_networking_nat_mask           = undef,
+  $lxc_networking_nat_network        = undef,
+  $lxc_networking_nat_dhcp_range     = undef,
+  $lxc_networking_nat_max_hosts      = undef,
   $lxc_networking_nat_update_dnsmasq = false
-  $lxc_networking_nat_dnsmasq_conf   = '/etc/dnsmasq.d/lxc'
+  $lxc_networking_nat_dnsmasq_conf   = undef,
   $network_default_conf              = '/etc/lxc/default.conf'
-  $network_nat_conf                  = '/etc/default/lxc-net'
-  $network_nat_service               = 'lxc-net'
+  $network_nat_conf                  = undef,
+  $network_nat_service               = undef,
 }
 
