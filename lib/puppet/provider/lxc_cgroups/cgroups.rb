107328a7e702648d12192598551c13783f3a4a39
Kevin Häfeli <kevin@zattoo.com>
2017-08-18 15:14:31 +0200

    bugfix and adding the getter and setter

--- lib/puppet/provider/lxc_cgroups/cgroups.rb
+++ lib/puppet/provider/lxc_cgroups/cgroups.rb
@@ -8,11 +8,13 @@ Puppet::Type.type(:lxc_cgroups).provide(:cgroups) do
   def create
     begin
       define_container
-      @container.set_cgroup_item("memory.limit_in_bytes", @resource[:memory)
+      @container.set_cgroup_item("memory.limit_in_bytes", @resource[:memory])
       @container.set_cgroup_item("cpuset.cpus", @resource[:cpuset])
-      @container.save_config    
+      @container.save_config
+      true
     rescue LXC::Error => e
     fail("Failed to create #{@resource[:name]}: #{e.message}")
+    false
     end
   end
 
@@ -26,9 +28,61 @@ Puppet::Type.type(:lxc_cgroups).provide(:cgroups) do
 
   # you can only set and get the values of a cgroup setting
   # it's not possible to unset it - so no destroy mode
-  def destory
+  def destroy
     Puppet.debug("It's not possible to absent cgroup values. please remove manually from config file")
     return false
   end
+
+  # getters and setters for memory and cpuset
+  def memory 
+    begin
+      @container.cgroup_item("memory.limit_in_bytes")
+    rescue LXC::Error => e
+      fail("Failed to get flags: #{e.message}")
+    end
+  end
   
+  def memory=(value)
+    begin
+      define_container
+      @container.set_cgroup_item("memory.limit_in_bytes", @resource[:memory])
+      @container.save_config
+    rescue LXC::Error => e
+      fail("Failed to set flags: #{e.message}")
+    end
+  end 
+
+  def cpuset
+    begin
+      @container.cgroup_item("cpuset.cpus")
+    rescue LXC::Error => e
+      fail("Failed to get flags: #{e.message}")
+    end
+  end
+  
+  def cpuset=(value)
+    begin
+      define_container
+      @container.set_cgroup_item("cpuset.cpus", @resource[:cpuset])
+      @container.save_config
+    rescue LXC::Error => e
+      fail("Failed to set flags: #{e.message}")
+    end
+  end 
+
+
+  private
+  def define_container
+    unless @container
+      @container = LXC::Container.new(@resource[:container])
+    end
+  end
+end
+
+  private
+  def define_container
+    unless @container
+      @container = LXC::Container.new(@resource[:container])
+    end
+  end
 end
